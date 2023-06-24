91330b2e0e9f404a238aaa075f3a94f3914280ff
pedrogsmalveira <99614364+pedrogsmalveira@users.noreply.github.com>
2023-06-24 16:06:13 -0300

    JTJTYJ

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+var i = setInterval(function () {
+    clearInterval(i);
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("conteudo").style.display = "inline";
+}, 4000);
